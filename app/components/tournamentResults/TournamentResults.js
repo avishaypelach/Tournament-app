b836102d6ac0379343ade8d1b45fddbad394c7bd
Avishay Pelach <avishay.pelach@gmail.com>
2017-08-03 16:13:13 +0300

    first commit

--- app/components/tournamentResults/TournamentResults.js
+++ app/components/tournamentResults/TournamentResults.js
@@ -13,18 +13,16 @@ export default class TournamentResults extends React.Component {
       sizePerPage: 10,
       searchFilter: null,
       levelFilter: 'All',
-      maxPlayers: 2000,
       players: [],
       offset: 1
     };
   }
 
-  // <!-- AJAX -->
-
+  // AJAX
   updateTournamentResults() {
     const xhr = new XMLHttpRequest();
     let start = (this.state.page - 1) * this.state.sizePerPage;
-    let n = this.state.sizePerPage +1;
+    let n = this.state.sizePerPage + 1;
     let search = this.state.searchFilter ? `&search=${this.state.searchFilter}` : '';
     let level = (this.state.levelFilter !== 'All') ? `&level=${this.state.levelFilter}` : '';
     xhr.open('GET', `http://localhost:20000/api/v1/players?start=${start}&n=${n}${search}${level}`, true);
@@ -40,31 +38,27 @@ export default class TournamentResults extends React.Component {
     xhr.send(null);
   }
 
-  // <!-- when app is uploading -->
-
+  // when app is uploading
   componentDidMount() {
     this.updateTournamentResults();
-    this.handlePaginationClick();
+    this.paginationClick();
   }
 
-  // <!-- handle search filter -->
-
-  handleSearchFilterChange(event) {
+  // handling search input
+  searchFilterChange(event) {
     this.setState({searchFilter: event.target.value, page: 1}, () => {
       this.updateTournamentResults();
     });
   }
 
-  // <!-- handle level filter  -->
-
-  handleLevelFilterChange(event) {
+  // handling players level filter
+  levelFilterChange(event) {
     this.setState({levelFilter: event.target.value, page: 1}, () => {
       this.updateTournamentResults();
     });
   }
 
-  // <!-- pagination -->
-
+  // moving to previous page
   prevPage() {
     this.setState({
       page: this.state.page - this.state.offset
@@ -73,6 +67,7 @@ export default class TournamentResults extends React.Component {
     })
   }
 
+  // moving to next page
   nextPage() {
     this.setState({
       page: this.state.page + this.state.offset
@@ -81,30 +76,32 @@ export default class TournamentResults extends React.Component {
     })
   }
 
-  handlePaginationClick(){
+  // ability to paginate with left and right keyboards
+  paginationClick() {
     document.addEventListener('keydown', (e) => {
-      if(e.keyCode === 37 && this.state.page > 1){
-        this.prevPage()
+      if (e.keyCode === 37 && this.state.page > 1) {
+        this.prevPage();
       }
-      if(e.keyCode === 39 && this.state.players.length === this.state.sizePerPage + 1){
-        this.nextPage()
+      if (e.keyCode === 39 && this.state.players.length === this.state.sizePerPage + 1) {
+        this.nextPage();
       }
     })
   }
 
+  // checking if user is on the first page, if so button "previous Page" is not showing
   isFirstPage() {
     return (this.state.page === 1) ? null :
       <Pager.Item previous href="#" onClick={() => this.prevPage()}>&larr; Previous Page</Pager.Item>
   }
 
+  // checking if players data is arriving, if not button "next page" is not showing
   isTherePlayers() {
     return (this.state.players.length < this.state.sizePerPage + 1) ? null :
       <Pager.Item next href="#" onClick={() => this.nextPage()}>Next
         Page &rarr;</Pager.Item>
   }
 
-  // <!-- app UI -->
-
+  // app UI
   render() {
     if (this.state.error) {
       return <div className="error"> Error! </div>;
@@ -116,9 +113,9 @@ export default class TournamentResults extends React.Component {
           <div className="filter-search">
             <input className="form-control filter-search-control" placeholder="Type to search.." type="text"
                    value={this.state.value}
-                   onChange={e => this.handleSearchFilterChange(e)}/>
+                   onChange={e => this.searchFilterChange(e)}/>
           </div>
-          <BootstrapTable data={this.state.players.slice(0,10)} striped hover>
+          <BootstrapTable data={this.state.players.slice(0, 10)} striped hover>
             <TableHeaderColumn isKey dataField='id'>ID</TableHeaderColumn>
             <TableHeaderColumn dataField='name' columnClassName='player-name'>Player Name
             </TableHeaderColumn>
@@ -126,7 +123,7 @@ export default class TournamentResults extends React.Component {
               <div className="filter-level-container">
                 <label className="filter-level-label">Level</label>
                 <select id="filter-level-control" className="filter-level form-control" value={this.state.value}
-                        onChange={e => this.handleLevelFilterChange(e)}>
+                        onChange={e => this.levelFilterChange(e)}>
                   <option>All</option>
                   <option>amateur</option>
                   <option>rookie</option>
